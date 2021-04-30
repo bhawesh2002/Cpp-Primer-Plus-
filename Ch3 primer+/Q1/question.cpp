6bbc783256b3f10c9f60148bff56806f30907507
Bhawesh Mankar <55053472+bhawesh2002@users.noreply.github.com>
2021-04-30 08:24:57 +0530

    Create question.cpp

---
+++ Ch3 primer+/Q1/question.cpp
@@ -0,0 +1,19 @@
+/*Write a short program that asks for your height in integer inches and then converts
+your height to feet and inches. Have the program use the underscore character to
+indicate where to type the response. Also use a const symbolic constant to represent
+the conversion factor.(1foot = 12inches)*/
+
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    int height;
+    const int inches_to_feet = 12;
+    cout << "Enter Your Height (In Inches)" << "___\b\b\b";
+    cin >> height;
+    int feet = height/12;
+    int inches = height%12;
+    cout << "Your Height in Feet is " << feet << " and in inches is " << inches << endl;
+    return 0;
+}
