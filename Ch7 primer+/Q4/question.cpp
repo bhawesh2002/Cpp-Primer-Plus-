033f9417616182a2db3e0487db26c15b751090d0
Bhawesh Mankar <55053472+bhawesh2002@users.noreply.github.com>
2021-04-30 08:36:57 +0530

    Create question.cpp

---
+++ Ch7 primer+/Q4/question.cpp
@@ -0,0 +1,18 @@
+/*Many state lotteries use a variation of the simple lottery portrayed by Listing 7.4. In
+these variations you choose several numbers from one set and call them the field
+numbers. For example, you might select five numbers from the field of 1–47).You
+also pick a single number (called a mega number or a power ball, etc.) from a second
+range, such as 1–27.To win the grand prize, you have to guess all the picks correctly.
+The chance of winning is the product of the probability of picking all the
+field numbers times the probability of picking the mega number. For instance, the
+probability of winning the example described here is the product of the probability
+of picking 5 out of 47 correctly times the probability of picking 1 out of 27 correctly.
+Modify Listing 7.4 to calculate the probability of winning this kind of lottery.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+}
