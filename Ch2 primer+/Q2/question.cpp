2b311a08b40d717c45e75d5a9283166752060f05
Bhawesh Mankar <55053472+bhawesh2002@users.noreply.github.com>
2021-04-30 08:20:01 +0530

    Add files via upload

--- Ch2 primer+/Q2/question.cpp
+++ Ch2 primer+/Q2/question.cpp
@@ -1,1 +1,21 @@
+//Write a C++ program that asks for a distance in furlongs and converts it to yards.
+//(One furlong is 220 yards.)
 
+#include <iostream>
+int  furlongs (int);
+
+int main(){
+    using namespace std;
+
+    int dis;
+    cout << "Furlongs to Yard's Converter" << endl;
+    cout << "Enter Distance in Furlongs = ";
+    cin >> dis;
+    int yards = furlongs(dis);
+    cout << "In Yard's = " << yards << endl;
+    return 0;
+}
+
+int furlongs(int f){
+    return f * 220;
+}
