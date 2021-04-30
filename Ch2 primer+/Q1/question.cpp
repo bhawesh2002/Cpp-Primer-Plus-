6907a0d9ed88d986fcb3865d09846c38333be385
Bhawesh Mankar <55053472+bhawesh2002@users.noreply.github.com>
2021-04-30 08:17:58 +0530

    Add files via upload

--- Ch2 primer+/Q1/question.cpp
+++ Ch2 primer+/Q1/question.cpp
@@ -1,1 +1,19 @@
+/*Write a C++ program that displays your name and address (or if you value your
+* privacy, a fictitious name and address)*/
 
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    string name;
+    string address;
+
+    cout << "enter your name and address" << "\n(Press enter after every entry)" << endl;
+    cout << "Name:-";
+    cin >> name;
+    cout << "Address:-";
+    cin >> address;
+
+    return 0;
+}
