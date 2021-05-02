29fd9a029c7b573ceb87fe71bb452f8b1a29927b
Bhawesh Mankar <55053472+bhawesh2002@users.noreply.github.com>
2021-05-02 07:06:58 +0530

    Update question.cpp

--- Ch5 primer+/Q2/question.cpp
+++ Ch5 primer+/Q2/question.cpp
@@ -15,19 +15,20 @@ return 0;
 }*/
 
 #include <iostream>
+#include <array>
+
+const int Arsize = 100;
 
 using namespace std;
 
 int main(){
-    int number;
-    long double factorials[number];
+    array<long double,Arsize> factorials;
     cout << "Enter a number" << endl;
-    cin >> number;
     factorials[0] = factorials[1] = 1LL;
-    for(int i = 2; i < number ; i++){
+    for(int i = 2; i <= Arsize ; i++){
         factorials[i] = i * factorials[i-1];
     } 
-    for(int i = 0; i < number; i++){
+    for(int i = 0; i < Arsize; i++){
         cout << i << "! = " << factorials[i] << endl; 
     } 
     return 0;
